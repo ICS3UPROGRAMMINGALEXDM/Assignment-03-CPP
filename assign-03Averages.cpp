cbdeb7d02269fd61c1fbea073bcaac6e96b0ea50
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-04-09 19:24:41 +0000

    completed assignment

--- assign-03Averages.cpp
+++ assign-03Averages.cpp
@@ -1,14 +1,20 @@
-#include "colors.h"
+// Copyright (c) 2022 Alex De Meo All rights reserved
+//
+// Created By: Alex De Meo
+// Date: 02//22
+// Description: Calculates the average of three numbers inputted by the user
+#include <cstdio>
 #include <iostream>
+#include "/Assignment-03-CPP/colors.h"
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::stoi;
+using std::stof;
 
 
 // Declaring variables
-string num1, num2, num3, answer;
 float average, num1Flt, num2Flt, num3Flt;
 bool restartLoop = true, answerLoop = true;
 
@@ -16,14 +22,20 @@ bool restartLoop = true, answerLoop = true;
 
 // function for the average calculations
 void calculate() {
-    cout << BOLDGREEN << "Hello";
-
+    average = (num1Flt + num2Flt + num3Flt) / 3;
+    printf("%sThe average of your three numbers is %.2f \n",
+        BOLDYELLOW, average);
 }
 
 
 int main() {
     // Loop to restart calculator
     while (restartLoop) {
+        string num1, num2, num3, answer;
+        // When the user says 'y' in answer this will be set to true
+        // so that the question about playing again will be asked
+        // again when it goes all through the program
+        answerLoop = true;
         // Get user input
         cout << BOLDWHITE << "Enter your first number: ";
         cin >> num1;
@@ -32,17 +44,43 @@ int main() {
         cout << "Enter your second number: ";
         cin >> num3;
 
+        // try is tp check to ensure a number was entered
         try {
-            num1Flt = stoi(num1);
-            num2Flt = stoi(num2);
-            num3Flt = stoi(num3);
-            
-            if (num1 >= 0
-                && num1 <= 100
-                && num2 >= 0
-                && num2 <= 100
-                && num3 >= 0
-                && num3 <= 100)
+            // casting to int
+            num1Flt = stof(num1);
+            num2Flt = stof(num2);
+            num3Flt = stof(num3);
+
+            if (num1Flt >= 0
+                && num1Flt <= 100
+                && num2Flt >= 0
+                && num2Flt <= 100
+                && num3Flt >= 0
+                && num3Flt <= 100) {
+                    calculate();
+                    while (answerLoop) {
+                        cout << BOLDWHITE <<
+                        "Would you like to calulate again? (y/n): ";
+                        cin >> answer;
+
+                        if (answer == "y") {
+                            cout << BOLDGREEN << "Okay" << endl;
+                            answerLoop = false;
+                        } else if (answer == "n") {
+                            cout << BOLDMAGENTA << "Okay" << endl;
+                            answerLoop = false;
+                            restartLoop = false;
+                        } else {
+                            cout << BOLDRED <<
+                            "I don't understand. Please try again." << endl;
+                        }
+                    }
+            } else {
+                cout << BOLDRED <<
+                "Numbers can't be less than 0 or more than 100" << endl;
+            }
+        } catch (...) {
+            cout << BOLDRED << "Invalid input, try again!" << endl;
         }
     }
 }
